603347f50d938175e73f01509808068cba3b23c4
hu.fox/胡佳_沪_开发 <fox.hu@51job.com>
2019-05-27 17:38:32 +0800

    Initial commit

---
+++ app/src/main/java/com/component/kotlintest/first/KotlinTest.kt
@@ -0,0 +1,15 @@
+package com.component.kotlintest.first
+
+class KotlinTest {
+    val a: String = "hello world"
+
+    //声明一个方法
+    fun sum(x: Int, y: Int): Int {
+        return x + y
+    }
+
+    //代码块函数体
+    fun sum1(x: Int, y: Int): Int = x + y
+
+    //val 和var的区别
+}
