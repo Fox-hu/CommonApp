423336821d2a7e878e67b9e7af54d2d8b7311d05
hu.fox/胡佳_沪_开发 <fox.hu@51job.com>
2019-05-27 17:59:23 +0800

    add demo

--- app/src/main/java/com/component/kotlintest/first/KotlinTest.kt
+++ app/src/main/java/com/component/kotlintest/first/KotlinTest.kt
@@ -11,5 +11,15 @@ class KotlinTest {
     //代码块函数体
     fun sum1(x: Int, y: Int): Int = x + y
 
-    //val 和var的区别
+    //val 和var的区别 val = var + final
+    val x = intArrayOf(1, 3, 2)
+
+    //kotlin中的函数是头等公民
+    //它允许以其他函数作为参数或返回值的函数
+    fun foo(x: Int) {
+        fun double(y: Int): Int {
+            return y * 2
+        }
+        println(double(x))
+    }
 }
