ed988159dcda6c0661a57f288ce3dea34ed4d743
hu.fox <fox.hu@51job.com>
2019-11-01 17:51:03 +0800

    实现cuview 手指滑动的view
    实现cuview 手指滑动的view

--- app/src/main/java/com/component/kotlintest/view/CuView.kt
+++ app/src/main/java/com/component/kotlintest/view/CuView.kt
@@ -2,6 +2,7 @@ package com.component.kotlintest.view
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.widget.LinearLayout
 import android.widget.Scroller
 
@@ -19,19 +20,37 @@ class CuView : LinearLayout {
 
     var isFirstFinish: Boolean = false
 
-    init {
-        val scroller = Scroller(context)
+
+    private val scroller = Scroller(context)
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        event?.let {
+            var action = event.action
+            when (action) {
+                MotionEvent.ACTION_DOWN -> {
+                    if (!isFirstFinish) {
+                        mStartX = event.rawX
+                        mStartY = event.rawY
+                    }
+                }
+
+                MotionEvent.ACTION_MOVE -> {
+                    scrollTo((mStartX - event.rawX) as Int, (mStartY - event.rawY) as Int)
+                }
+
+                MotionEvent.ACTION_UP -> {
+                    isFirstFinish = true
+                }
+            }
+        }
+        return true
     }
 
-//    override fun onTouchEvent(event: MotionEvent?): Boolean {
-//        var action = event?.action
-//        when (action) {
-//            MotionEvent.ACTION_DOWN -> {
-//
-//            }
-//            else -> {
-//            }
-//        }
-//    }
-
+    override fun computeScroll() {
+        if (scroller.computeScrollOffset()) {
+            scrollTo(scroller.currX, scroller.currY)
+            invalidate()
+        }
+        super.computeScroll()
+    }
 }
