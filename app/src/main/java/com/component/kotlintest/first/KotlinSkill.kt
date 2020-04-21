db204419cf66dfe60826652fa4aa5f57f35c1016
hu.fox <fox.hu@51job.com>
2020-04-21 17:56:25 +0800

    添加责任链模式和观察模式的高级用法
    添加责任链模式和观察模式的高级用法

--- app/src/main/java/com/component/kotlintest/first/KotlinSkill.kt
+++ app/src/main/java/com/component/kotlintest/first/KotlinSkill.kt
@@ -9,21 +9,20 @@ import kotlin.properties.Delegates
  * @Author fox
  * @Date 2020/1/15 22:32
  */
-class KotlinSkill {
 
-    //将方法作为map的value 调用时直接使用invoke进行调用
-    private val keymap: Map<Int, () -> Unit> = mapOf(
-        0 to { print(0) },
-        1 to { print(1) },
-        2 to { print(2) }
-    )
 
-    fun keymapInvoke(key: Int) {
-        keymap[key]?.invoke()
-    }
+//1.将方法作为map的value 调用时直接使用invoke进行调用
+private val keymap: Map<Int, () -> Unit> = mapOf(
+    0 to { print(0) },
+    1 to { print(1) },
+    2 to { print(2) }
+)
+
+fun keymapInvoke(key: Int) {
+    keymap[key]?.invoke()
 }
 
-//重载companion object的invoke 简化版的工厂模式
+//2.重载companion object的invoke 简化版的工厂模式
 //invoke的含义是将对象作为函数进行调用
 interface Iface1<E> : List<E> {
     companion object Factory {
@@ -46,20 +45,47 @@ interface Iface1<E> : List<E> {
 val iface1 = Iface1<Any>(1)
 val iface2 = Iface1<Any>("hello")
 
-//属性委托 数据观察者模式
-class User4 {
-    var name: String by Delegates.observable("no name") { d, old, new ->
-        println("$old - $new")
+//3.属性委托 数据观察者模式
+interface StockUpdateListener {
+    fun onRise(price: Int)
+    fun onFall(price: Int)
+}
+
+class StockDisplay : StockUpdateListener {
+
+    override fun onFall(price: Int) {
+        println("The stock price has fell to $price")
+    }
+
+    override fun onRise(price: Int) {
+        println("The stock price has risen to $price")
+    }
+
+}
+
+class StockUpdate {
+    val listeners = mutableSetOf<StockUpdateListener>()
+
+    var price: Int by Delegates.observable(0) { _, old, new ->
+        listeners.forEach {
+            if (new > old) it.onRise(price) else it.onFall(price)
+        }
     }
 }
 
-//属性可以使用map进行初始化
+//使用vetoable来拦截观察对象
+var value: Int by Delegates.vetoable(0) { _, old, new ->
+    //与observable不同的是 只有new的值大于0才会被接受 否则会被截获
+    new > 0
+}
+
+//4.属性可以使用map进行初始化
 class User5(val map: Map<String, Any?>) {
     val name: String by map
     val age: Int by map
 }
 
-//invoke可以作为必须重写的方法使用
+//5.invoke可以作为必须重写的方法使用
 data class Issue(
     val id: String,
     val project: String,
@@ -79,4 +105,65 @@ class ImportIssuesPredicate(val project: String) : (Issue) -> Boolean {
     }
 }
 
+//6.内联函数创建抽象工厂
+interface Computer
+
+class Dell : Computer
+class Asus : Computer
+class Acer : Computer
+
+abstract class AbstractFactory {
+
+    abstract fun produce(): Computer
+
+    companion object {
+        inline operator fun <reified T : Computer> invoke(): AbstractFactory =
+//注意 这个地方是T::class 不是T::class.java
+            when (T::class) {
+                Dell::class -> DellFactory()
+                Asus::class -> AsusFactory()
+                Acer::class -> AcerFactory()
+                else -> throw IllegalArgumentException()
+            }
+    }
+}
+
+class DellFactory : AbstractFactory() {
+    override fun produce(): Computer = Dell()
+}
+
+class AsusFactory : AbstractFactory() {
+    override fun produce(): Computer = Asus()
+}
+
+class AcerFactory : AbstractFactory() {
+    override fun produce(): Computer = Acer()
+}
+
+//7.lambda实现责任链
+class PartialFunction<in P1, out R>(
+    private val defineAt: (P1) -> Boolean,
+    private val f: (P1) -> R
+) : (P1) -> R {
+    override fun invoke(p1: P1): R {
+        if (defineAt(p1)) {
+            return f(p1)
+        } else {
+            throw java.lang.IllegalArgumentException("Value: ($p1) isn't support by this function")
+        }
+    }
+
+    fun isDefinedAt(p1: P1) = defineAt(p1)
+}
+
+infix fun <P1, R> PartialFunction<P1, R>.orElse(that: PartialFunction<P1, R>): PartialFunction<P1, R> {
+    return PartialFunction({
+        this.isDefinedAt(it) || that.isDefinedAt(it)
+    }) {
+        when {
+            this.isDefinedAt(it) -> this(it)
+            else -> that(it)
+        }
+    }
+}
 
--- app/src/test/java/com/component/kotlintest/Test.kt
+++ app/src/test/java/com/component/kotlintest/Test.kt
@@ -1,8 +1,6 @@
 package com.component.kotlintest
 
-import com.component.kotlintest.first.KotlinSkill
-import com.component.kotlintest.first.User4
-import com.component.kotlintest.first.User5
+import com.component.kotlintest.first.*
 import org.junit.Test
 
 class Test {
@@ -25,19 +23,63 @@ class Test {
 
     @Test
     fun test3() {
-        val kotlinSkill = KotlinSkill()
-        kotlinSkill.keymapInvoke(1)
+        keymapInvoke(1)
     }
 
     @Test
     fun test4() {
-        val user4 = User4()
-        user4.name = "fox"
+        val stockUpdate = StockUpdate()
+        val stockDisplay = StockDisplay()
+        stockUpdate.listeners.add(stockDisplay)
+        stockUpdate.price = 100
+        stockUpdate.price = 98
     }
 
     @Test
     fun test5() {
         val user5 = User5(mapOf("name" to "fox", "age" to 29))
         println("name = ${user5.name},age = ${user5.age}")
+    }
+
+
+    @Test
+    fun test6() {
+        val abstractFactory = AbstractFactory<Dell>()
+        print(abstractFactory.produce())
+    }
+
+    @Test
+    fun test7() {
+        //好好理解这个
+        data class ApplyEvent(val money: Int, val title: String)
+
+        val groupLeader = {
+            val defineAt: (ApplyEvent) -> Boolean = { it.money <= 200 }
+            val handler: (ApplyEvent) -> Unit =
+                { println("GroupLeader handled application:${it.title}") }
+            PartialFunction(defineAt, handler)
+        }()
+
+        val president = {
+            val defineAt: (ApplyEvent) -> Boolean = { it.money <= 500 }
+            val handler: (ApplyEvent) -> Unit =
+                { println("President handled application:${it.title}") }
+            PartialFunction(defineAt, handler)
+        }()
+
+        val college = {
+            val defineAt: (ApplyEvent) -> Boolean = { true }
+            val handler: (ApplyEvent) -> Unit =
+                {
+                    when {
+                        it.money > 1000 -> println("College: This application is refused.")
+                        else -> println("College handled application:${it.title}")
+                    }
+                }
+            PartialFunction(defineAt, handler)
+        }()
+
+        val applyChain = groupLeader orElse president orElse college
+        applyChain(ApplyEvent(2000, "hold a debate match"))
     }
 }
