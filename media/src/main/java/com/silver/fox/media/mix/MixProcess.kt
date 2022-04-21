03904b4479d83200ddb05308a82d7a5713dd1e5f
hujia <hujia0523@gmail.com>
2022-04-21 23:54:20 +0800

    为一键剪辑demo添加注释

--- media/src/main/java/com/silver/fox/media/mix/MixProcess.kt
+++ media/src/main/java/com/silver/fox/media/mix/MixProcess.kt
@@ -13,6 +13,7 @@ import java.nio.ByteBuffer
 object MixProcess {
     private const val TAG = "MusicProcess"
     private const val TIMEOUT = 1000
+    //将一个视频和一个音频进行混合
     fun mixAudioTrack(
         videoInput: String?,
         audioInput: String?,
@@ -21,18 +22,19 @@ object MixProcess {
         videoVolume: Int,
         aacVolume: Int
     ) {
+        //1.将mp3和mp4解封装，转换为pcm格式，合并转换后的pcm，封装为wav格式
         val cacheDir = File("${InitApp.CONTEXT.filesDir?.parentFile?.path}")
-        //        下载下来的音乐转换城pcm
         val aacPcmFile = File(cacheDir, "audio" + ".pcm")
-        //        视频自带的音乐转换城pcm
         val videoPcmFile = File(cacheDir, "video" + ".pcm")
         val audioExtractor = MediaExtractor()
         audioExtractor.setDataSource(audioInput!!)
-        decodeToPCM(
-            videoInput, videoPcmFile.absolutePath,
+        //抽取mp4的音频 转为video.pcm
+        decodeToPCM(videoInput, videoPcmFile.absolutePath,
             startTimeUs, endTimeUs
         )
+        //抽取mp3的音频 转为audio.pcm
         decodeToPCM(audioInput, aacPcmFile.absolutePath, startTimeUs, endTimeUs)
+        //将audio.pcm和video.pcm 混合后 转为.wav格式 注意 此时并未进行编码
         val adjustedPcm = File(cacheDir, "混合后的" + ".pcm")
         PcmUtils.mixPcm(
             videoPcmFile.absolutePath, aacPcmFile.absolutePath,
@@ -46,7 +48,7 @@ object MixProcess {
             adjustedPcm.absolutePath, wavFile.absolutePath
         )
         Log.i(TAG, "mixAudioTrack: 转换完毕")
-        //混音的wav文件   + 视频文件   ---》  生成
+        //2.将wav文件 与mp4文件 合成为新的out.mp4文件
         mixVideoAndMusic(videoInput, output, startTimeUs, endTimeUs, wavFile)
     }
 
@@ -57,47 +59,40 @@ object MixProcess {
         endTimeUs: Int?,
         wavFile: File
     ) {
-
-
-        //        初始化一个视频封装容器
+        //1、设置合成器，确定合成器两个轨道的各项参数，像轨道中分别写入视频和音频数据
+        // 将输入的mp4和wav文件重新封装成一个mp4文件
         val mediaMuxer = MediaMuxer(output, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4)
-
-//            一个轨道    既可以装音频 又视频   是 1 不是2
-//            取音频轨道  wav文件取配置信息
-//            先取视频
+        // 设置视频输入轨道
         val mediaExtractor = MediaExtractor()
         mediaExtractor.setDataSource(videoInput!!)
-        //            拿到视频轨道的索引
+        // 拿到视频轨道的索引
         val videoIndex = selectTrack(mediaExtractor, false)
         val audioIndex = selectTrack(mediaExtractor, true)
 
 
-//            视频配置 文件
+        // 视频配置 文件
         val videoFormat = mediaExtractor.getTrackFormat(videoIndex)
-        //开辟了一个 轨道   空的轨道   写数据     真实
+        // 开辟空的轨道写视频数据
         mediaMuxer.addTrack(videoFormat)
 
-//        ------------音频的数据已准备好----------------------------
-//            视频中音频轨道   应该取自于原视频的音频参数
+        // 音频的数据已准备好 由wav文件提供数据 音频各项参数取自于原mp4的音频参数
         val audioFormat = mediaExtractor.getTrackFormat(audioIndex)
         val audioBitrate = audioFormat.getInteger(MediaFormat.KEY_BIT_RATE)
         audioFormat.setString(MediaFormat.KEY_MIME, MediaFormat.MIMETYPE_AUDIO_AAC)
-        //        添加一个空的轨道  轨道格式取自 视频文件，跟视频所有信息一样
+        // 开辟空轨道写音频数据 轨道格式取自原mp4文件 与原mp4文件保持一致
         val muxerAudioIndex = mediaMuxer.addTrack(audioFormat)
 
-
-//            音频轨道开辟好了  输出开始工作
+        // 音频、视频两个轨道开辟完成  输出开始工作
         mediaMuxer.start()
 
-//音频的wav
+        //2、解wav文件封装，编码为aac数据，写入合成器的音频轨道
         val pcmExtrator = MediaExtractor()
         pcmExtrator.setDataSource(wavFile.absolutePath)
         val audioTrack = selectTrack(pcmExtrator, true)
         pcmExtrator.selectTrack(audioTrack)
         val pcmTrackFormat = pcmExtrator.getTrackFormat(audioTrack)
 
-
-        //最大一帧的 大小
+        //最大一帧的大小
         var maxBufferSize = 0
         maxBufferSize = if (audioFormat.containsKey(MediaFormat.KEY_MAX_INPUT_SIZE)) {
             pcmTrackFormat.getInteger(MediaFormat.KEY_MAX_INPUT_SIZE)
@@ -105,26 +100,23 @@ object MixProcess {
             100 * 1000
         }
 
-
-//    最终输出   后面   混音   -----》     重采样   混音     这个下节课讲
+        // 设置编码器
         val encodeFormat = MediaFormat.createAudioFormat(
             MediaFormat.MIMETYPE_AUDIO_AAC,
             44100, 2
         ) //参数对应-> mime type、采样率、声道数
         encodeFormat.setInteger(MediaFormat.KEY_BIT_RATE, audioBitrate) //比特率
-        //            音质等级
+        // 音质等级
         encodeFormat.setInteger(
             MediaFormat.KEY_AAC_PROFILE,
             MediaCodecInfo.CodecProfileLevel.AACObjectLC
         )
-        //            解码  那段
         encodeFormat.setInteger(MediaFormat.KEY_MAX_INPUT_SIZE, maxBufferSize)
-        //解码 那
         val encoder = MediaCodec.createEncoderByType(MediaFormat.MIMETYPE_AUDIO_AAC)
-        //            配置AAC 参数  编码 pcm   重新编码     视频文件变得更小
+        //重新编码 将pcm数据编码为aac数据
         encoder.configure(encodeFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE)
         encoder.start()
-        //            容器
+
         var buffer = ByteBuffer.allocateDirect(maxBufferSize)
         val info = MediaCodec.BufferInfo()
         var encodeDone = false
@@ -133,7 +125,7 @@ object MixProcess {
             if (inputBufferIndex >= 0) {
                 val sampleTime = pcmExtrator.sampleTime
                 if (sampleTime < 0) {
-//                        pts小于0  来到了文件末尾 通知编码器  不用编码了
+                //pts小于0  来到了文件末尾 通知编码器  不用编码了
                     encoder.queueInputBuffer(
                         inputBufferIndex,
                         0,
@@ -143,19 +135,18 @@ object MixProcess {
                     )
                 } else {
                     val flags = pcmExtrator.sampleFlags
-                    //
                     val size = pcmExtrator.readSampleData(buffer, 0)
-                    //                    编辑     行 1 还是不行 2   不要去用  空的
                     val inputBuffer = encoder.getInputBuffer(inputBufferIndex)
                     inputBuffer!!.clear()
                     inputBuffer.put(buffer)
                     inputBuffer.position(0)
                     encoder.queueInputBuffer(inputBufferIndex, 0, size, sampleTime, flags)
-                    //                        读完这一帧
+                    // 读完这一帧 继续分离wav文件下一帧音频数据
                     pcmExtrator.advance()
+                    Log.i(TAG,"写入一帧音频数据成功 , size = $size, sampleTime = $sampleTime")
                 }
             }
-            //                获取编码完的数据
+            // 获取编码完的数据
             var outputBufferIndex = encoder.dequeueOutputBuffer(info, TIMEOUT.toLong())
             while (outputBufferIndex >= 0) {
                 if (info.flags == MediaCodec.BUFFER_FLAG_END_OF_STREAM) {
@@ -163,23 +154,25 @@ object MixProcess {
                     break
                 }
                 val encodeOutputBuffer = encoder.getOutputBuffer(outputBufferIndex)
-                //                    将编码好的数据  压缩 1     aac
+                // 将编码好的acc数据写入合成器的音频轨道中
                 mediaMuxer.writeSampleData(muxerAudioIndex, encodeOutputBuffer!!, info)
                 encodeOutputBuffer.clear()
                 encoder.releaseOutputBuffer(outputBufferIndex, false)
                 outputBufferIndex = encoder.dequeueOutputBuffer(info, TIMEOUT.toLong())
             }
         }
-        //    把音频添加好了
+        // 以上 就把合成器中的音频轨道数据添加好了
+        Log.i(TAG,"写入全部音频数据成功")
+        // 继续向合成器中添加视频轨道数据
         if (audioTrack >= 0) {
             mediaExtractor.unselectTrack(audioTrack)
         }
-        //视频
+        //选择mp4中的视频轨道
         mediaExtractor.selectTrack(videoIndex)
         mediaExtractor.seekTo(startTimeUs.toLong(), MediaExtractor.SEEK_TO_PREVIOUS_SYNC)
         maxBufferSize = videoFormat.getInteger(MediaFormat.KEY_MAX_INPUT_SIZE)
         buffer = ByteBuffer.allocateDirect(maxBufferSize)
-        //封装容器添加视频轨道信息
+        //向合成器中添加视频轨道信息 无需解码 直接添加h264数据
         while (true) {
             val sampleTimeUs = mediaExtractor.sampleTime
             if (sampleTimeUs == -1L) {
@@ -192,18 +185,21 @@ object MixProcess {
             if (endTimeUs != null && sampleTimeUs > endTimeUs) {
                 break
             }
-            //                pts      0
+            // 这里的pts就不是原来的sampleTimeUs了 需要 - 裁剪开始时间 + 余量 此处取600Us
             info.presentationTimeUs = sampleTimeUs - startTimeUs + 600
             info.flags = mediaExtractor.sampleFlags
-            //                读取视频文件的数据  画面 数据   压缩1  未压缩2
+            // 使用解封装器 读取视频文件的视频数据 h264格式 写入buffer中
             info.size = mediaExtractor.readSampleData(buffer, 0)
             if (info.size < 0) {
                 break
             }
-            //                视频轨道  画面写完了
+            // 使用buffer 向合成器中视频轨道写入视频数据
             mediaMuxer.writeSampleData(videoIndex, buffer, info)
+            // 解封装器继续读取下一帧的内容
             mediaExtractor.advance()
+            Log.i(TAG,"写入一帧视频数据成功 , size = ${info.size} pts = ${info.presentationTimeUs}")
         }
+        Log.i(TAG,"写入全部视频数据成功")
         try {
             pcmExtrator.release()
             mediaExtractor.release()
@@ -214,19 +210,17 @@ object MixProcess {
         }
     }
 
-    //    MP3 截取并且输出  pcm
+    //    从一个MP3文件中截取并且输出pcm文件
     @SuppressLint("WrongConstant")
     fun decodeToPCM(musicPath: String?, outPath: String?, startTime: Int, endTime: Int) {
         if (endTime < startTime) {
             return
         }
-        //    MP3  （zip  rar    ） ----> aac   封装个事 1   编码格式
-//        jie  MediaExtractor = 360 解压 工具
+        // 先用解封装器 将mp3文件中的音频文件分离出来
         val mediaExtractor = MediaExtractor()
         mediaExtractor.setDataSource(musicPath!!)
         val audioTrack = selectTrack(mediaExtractor, true)
         mediaExtractor.selectTrack(audioTrack)
-        // 视频 和音频
         mediaExtractor.seekTo(startTime.toLong(), MediaExtractor.SEEK_TO_CLOSEST_SYNC)
         // 轨道信息  都记录 编码器
         val audioFormat = mediaExtractor.getTrackFormat(audioTrack)
@@ -237,16 +231,18 @@ object MixProcess {
             100 * 1000
         }
         val buffer = ByteBuffer.allocateDirect(maxBufferSize)
-        //        h264   H265  音频
+        // 创建解码器 将aac 解码为pcm文件
         val mediaCodec = MediaCodec.createDecoderByType(
             audioFormat
                 .getString(MediaFormat.KEY_MIME)
         )
-        //        设置解码器信息    直接从 音频文件
+        // 设置解码器信息
         mediaCodec.configure(audioFormat, null, null, 0)
         val pcmFile = File(outPath)
         val writeChannel = FileOutputStream(pcmFile).channel
+        // 开始解码
         mediaCodec.start()
+
         val info = MediaCodec.BufferInfo()
         var outputBufferIndex = -1
         while (true) {
@@ -256,23 +252,20 @@ object MixProcess {
                 if (sampleTimeUs == -1L) {
                     break
                 } else if (sampleTimeUs < startTime) {
-//                    丢掉 不用了
+                    // 不在裁剪时间内 舍弃不处理 解封装器继续处理下一帧
                     mediaExtractor.advance()
                     continue
                 } else if (sampleTimeUs > endTime) {
                     break
                 }
-                //                获取到压缩数据
+                // 从解封装器中 获取到压缩数据
                 info.size = mediaExtractor.readSampleData(buffer, 0)
                 info.presentationTimeUs = sampleTimeUs
                 info.flags = mediaExtractor.sampleFlags
 
-//                下面放数据  到dsp解码
+                // 放入解码器输入队列
                 val content = ByteArray(buffer.remaining())
                 buffer[content]
-                //                输出文件  方便查看
-//                FileUtils.writeContent(content);
-//                解码
                 val inputBuffer = mediaCodec.getInputBuffer(decodeInputIndex)
                 inputBuffer!!.put(content)
                 mediaCodec.queueInputBuffer(
@@ -282,13 +275,15 @@ object MixProcess {
                     info.presentationTimeUs,
                     info.flags
                 )
-                //                释放上一帧的压缩数据
+                // 解封装器继续处理下一帧数据
                 mediaExtractor.advance()
             }
+            //获取解码后的数据
             outputBufferIndex = mediaCodec.dequeueOutputBuffer(info, 1000)
             while (outputBufferIndex >= 0) {
                 val decodeOutputBuffer = mediaCodec.getOutputBuffer(outputBufferIndex)
-                writeChannel.write(decodeOutputBuffer) //MP3  1   pcm2
+                // 写入到输出文件
+                writeChannel.write(decodeOutputBuffer)
                 mediaCodec.releaseOutputBuffer(outputBufferIndex, false)
                 outputBufferIndex = mediaCodec.dequeueOutputBuffer(info, 1000)
             }
@@ -297,16 +292,10 @@ object MixProcess {
         mediaExtractor.release()
         mediaCodec.stop()
         mediaCodec.release()
-        //        转换MP3    pcm数据转换成mp3封装格式
-//
-//        File wavFile = new File(Environment.getExternalStorageDirectory(),"output.mp3" );
-//        new PcmToWavUtil(44100,  AudioFormat.CHANNEL_IN_STEREO,
-//                2, AudioFormat.ENCODING_PCM_16BIT).pcmToWav(pcmFile.getAbsolutePath()
-//                , wavFile.getAbsolutePath());
-        Log.i("David", "mixAudioTrack: 转换完毕")
+        Log.i(TAG,"mp3 $musicPath 转 pcm: $outPath 转换完毕")
     }
 
-    fun selectTrack(extractor: MediaExtractor, audio: Boolean): Int {
+    private fun selectTrack(extractor: MediaExtractor, audio: Boolean): Int {
         val numTracks = extractor.trackCount
         for (i in 0 until numTracks) {
             val format = extractor.getTrackFormat(i)
